dad5c40723b80c808826036cfaab6b6360bb0eaa
Paul Newby <paulnewby@Pauls-iMac.home>
2020-08-04 17:04:38 +0100

    Updates

---
+++ precache-manifest.a2f7e44f7395479c8f80ba5865547f40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ee41c9d46e5063c611c81463043343",
+    "url": "/QuoteMachine/index.html"
+  },
+  {
+    "revision": "f02100d9f437c921d4f0",
+    "url": "/QuoteMachine/static/css/2.5be323c2.chunk.css"
+  },
+  {
+    "revision": "5d016d13b52d561ff99a",
+    "url": "/QuoteMachine/static/css/main.d529f060.chunk.css"
+  },
+  {
+    "revision": "f02100d9f437c921d4f0",
+    "url": "/QuoteMachine/static/js/2.dd6a0c61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuoteMachine/static/js/2.dd6a0c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d016d13b52d561ff99a",
+    "url": "/QuoteMachine/static/js/main.0bbc9da0.chunk.js"
+  },
+  {
+    "revision": "ec43c49c29691e372cf0",
+    "url": "/QuoteMachine/static/js/runtime-main.d464e597.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/QuoteMachine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/QuoteMachine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/QuoteMachine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/QuoteMachine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/QuoteMachine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
